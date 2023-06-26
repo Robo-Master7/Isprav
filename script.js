c89246b62400cf3687e1170ee0834bf32213f4e6
Robo-Master7 <136693201+Robo-Master7@users.noreply.github.com>
2023-06-26 13:35:48 +0300

    0.1

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict'
+
+// about
+
+let aboutMore = document.querySelector('.about-more');
+let aboutHidden = document.querySelector('.about-hidden');
+
+aboutMore.addEventListener('click', () => {
+  aboutHidden.classList.remove('about-hidden');
+  aboutMore.closest('p').classList.add('about-hidden');
+});
+
+// ьщвфд
+
+let orderButton = document.querySelector('.order-button');
+let overlay = document.querySelector('.overlay');
+let closeButton = document.querySelector('.close-button');
+
+orderButton.addEventListener('click', () => {
+  overlay.classList.remove('hidden');
+});
+
+closeButton.addEventListener('click', () => {
+  overlay.classList.add('hidden');
+});
+
+// swiper
+
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
